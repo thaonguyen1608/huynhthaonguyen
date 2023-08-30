92079bcfc7c519a6e0f904f45bfb96dcee5da993
huynhthaonguyen <nuyenml0984@gmail.com>
2023-08-30 16:05:38 +0700

    add file

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Tamgiac tamGiac1 = new Tamgiac(4, 5, 6); // Tam giác thường
+            Tamgiac tamGiac2 = new Tamgiac(-1, 2, 3); // Hình vi phạm ràng buộc
+            Tamgiac tamGiac3 = new Tamgiac(8, 8, 8); // Tam giác đều
+            Tamgiac tamGiac4 = new Tamgiac(7, 5, 7); // Tam giác cân
+            Tamgiac tamGiac5 = new Tamgiac(9, 10, 13); // Tam giác thường
+
+            Tamgiac[] danhSachTamGiac = { tamGiac1, tamGiac2, tamGiac3, tamGiac4, tamGiac5 };
+
+            Console.WriteLine("{0,-15}{1,-20}{2,-10}{3,-10}", "Tam giác", "Kiểu tam giác", "Chu vi", "Diện tích");
+            Console.WriteLine(new string('-', 55));
+            for (int i = 0; i < danhSachTamGiac.Length; i++)
+            {
+                Console.WriteLine("Tam giác {0}: {1}", i + 1, danhSachTamGiac[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tamgiac.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Tamgiac
+    {
+        private double ma, mb, mc;
+
+    public Tamgiac()
+    {
+        ma = mb = mc = 0;
+    }
+
+    public Tamgiac(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a))
+        {
+            ma = mb = mc = 0;
+        }
+        else
+        {
+            ma = a;
+            mb = b;
+            mc = c;
+        }
+    }
+
+    public double Ma
+    {
+        get { return ma; }
+        set
+        {
+            if (value > 0 && value + mb > mc && value + mc > mb)
+                ma = value;
+        }
+    }
+
+    public double Mb
+    {
+        get { return mb; }
+        set
+        {
+            if (value > 0 && value + ma > mc && value + mc > ma)
+                mb = value;
+        }
+    }
+
+    public double Mc
+    {
+        get { return mc; }
+        set
+        {
+            if (value > 0 && value + ma > mb && value + mb > ma)
+                mc = value;
+        }
+    }
+
+    public double TinhChuVi()
+    {
+        return ma + mb + mc;
+    }
+
+    public double TinhDienTich()
+    {
+        double p = TinhChuVi() / 2;
+        return Math.Sqrt(p * (p - ma) * (p - mb) * (p - mc));
+    }
+
+    public string KieuTamGiac()
+    {
+        if (ma == mb && mb == mc)
+            return "Tam giác đều";
+        else if (ma == mb || mb == mc || ma == mc)
+            return "Tam giác cân";
+        else if (ma != mb && mb != mc && ma != mc)
+            return "Tam giác thường";
+        else
+            return "Không phải tam giác";
+    }
+
+    public override string ToString()
+    {
+        return ("Thông tin 3 cạnh: {ma}, {mb}, {mc}\nKiểu tam giác: {KieuTamGiac()}\nChu vi: {TinhChuVi()}\nDiện tích: {TinhDienTich()}");
+    }
+}
+
+    
+}
+
